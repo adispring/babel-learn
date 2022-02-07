45391ad411dc6d5bbbc2a91666374a50841726e4
wangzengdi <sunnyadi@163.com>
2022-02-07 14:44:45 +0800

    feat: 箭头函数转换

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-const square = n => n * n
-console.log(square(2))
-
-function addAll () {
-  return Array.from(arguments).reduce(function (a, b) {
-    return a + b
-  })
-}
-
-const all = addAll(1, 2, 3)
-console.log(all)
+const fn = () => 1;
